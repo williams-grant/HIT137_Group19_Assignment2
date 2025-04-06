dc902342cd0c59edc7a602adbccf492f9a07d82d
zaliturner <zaliturner@yahoo.com>
2025-04-06 16:23:18 +1000

    Update a2q1.py

--- a2q1.py
+++ a2q1.py
@@ -6,18 +6,12 @@ def encrypt(text, n, m):
     for char in text: 
         if 'a' <= char <= 'z':
             if 'a' <= char <= 'm':
-                encrypted_char = chr(((ord(char) - ord('a') + n * m) % 26) + ord('a'))
+                new_char = ord(char) + (n * m)
+                new_char = (new_char - ord('a')) % 26 + ord('a')
+        else:
+            new_char = ord(char) + (n + m)
+            new_char = (new_char - ord('a')) % 26 + ord('a')
 
-            else: 
-                encrypted_char = chr(((ord(char) - ord('a') - (n + m)) % 26) + ord('a'))
-
-        elif 'A' <= char <= 'Z':
-            if 'A' <= char <= 'M':
-                encrypted_char = chr(((ord(char) - ord('A') - n) % 26) + ord('A'))
-            else: 
-                encrypted_char = chr(((ord(char) - ord('A') + m ** 2) % 26) + ord('A'))       
-
-                
 
 
 

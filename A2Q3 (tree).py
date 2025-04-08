34374657c3281776abd4c20a2028a7369e035752
williams-grant <grant0421@gmail.com>
2025-04-08 19:48:03 +1000

    Update A2Q3 (tree).py
    Feedback added as comments with 3 hashes

--- A2Q3 (tree).py
+++ A2Q3 (tree).py
@@ -1,17 +1,17 @@
 import turtle
 
-def draw_tree(branch_length, angle_left, angle_right, reduction_factor):
-    if reduction_factor == 0:
+def draw_tree(branch_length, angle_left, angle_right, reduction_factor): #Needs one more variable for number of branches remaining
+    if reduction_factor == 0: ###This needs to be testing number of branches remaining
         return
 
     turtle.forward(branch_length)
     turtle.left(angle_left)
 
-    draw_tree(branch_length * reduction_factor, angle_left, angle_right, reduction_factor)
+    draw_tree(branch_length * reduction_factor, angle_left, angle_right, reduction_factor) ###with additional variable for number of branches remaining, add this to call as number of branches - 1
 
     turtle.right(angle_left + angle_right)
 
-    draw_tree(branch_length * reduction_factor, angle_left, angle_right, reduction_factor)
+    draw_tree(branch_length * reduction_factor, angle_left, angle_right, reduction_factor) ###with additional variable for number of branches remaining, add this to call as number of branches - 1
 
     turtle.left(angle_right)
     turtle.backward(branch_length)
@@ -22,6 +22,7 @@ def main():
     angle_right = float(input("Enter right branch angle: "))
     branch_length = float(input("Enter starting branch length: "))
     reduction_factor = float(input("Enter branch length reduction factor (e.g., 0.7 for 70%): "))
+    ###Need one more input for number of branches (recursion depth)
 
     # Setting up turtle
     turtle.speed("fastest")

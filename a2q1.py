b1f988e1fcb3b22ee9cb5b8048a0b17ecc9475d1
zaliturner <zaliturner@yahoo.com>
2025-04-09 13:53:17 +1000

    Update a2q1.py

--- a2q1.py
+++ a2q1.py
@@ -1,30 +1,29 @@
-file = open("raw_text.txt", "r")
 def encrypt(text, n, m):
     encrypted_text = ""
 
-# Don't use char as variable name as char is also a function you will need to use
-# Can use something like char_to_encrypt
 for char_to_encrypt in text: 
-        # If you structure your if statements like this, then you don't need to nest them.
-        # if 'a' <= char <= 'm':
-        # elif 'n' <= char <= 'z':
-        # elif 'A' <= char <= 'M':
-        # elif 'N' <= char <= 'Z':
-        # else:
-        #    new_char = char
-        # Note: else is required because non-letter characters need to be retained e.g. spaces, full stops etc
-        if 'a' <= char_to_encrypt <= 'z':
-            if 'a' <= char <= 'm':
-                new_char = char(ord(a) + ord(char_to_encrypt) - ord(a) + (n * m) ) % 26)
-                # This one can be done with one line: new_char = char(ord(a) + (ord(char_to_encrypt) - ord(a) + (n * m)) % 26)
-        else:
-            new_char = ord(char_to_encrypt) + (n + m) 
-            new_char = (new_char - ord('a')) % 26 + ord('a')
-            # This one needs to shift backwards and there are two scenarios:
-            # 1) The shift back stays within the range of a-z
-            #        can be tested by ord(char_to_encrypt) - ord(a) - ((n + m) % 26) >= 0
-            #        then calculated as new_char = char(ord(char_to_encrypt) - ((n + m) % 26))
-            # 2) The shift back goes below the range of a-z (this would be the else to the above test)
-            #        then calculated as new_char = char(ord(char_to_encrypt) + 26 - ((n + m) % 26))
+    if 'a' <= char_to_encrypt <= 'm':
+        new_char = chr(ord('a') + ord(char_to_encrypt) - ord('a') + (n * m)) % 26)
+   
+    elif 'n' <= char_to_encrypt <= 'z': 
+            new_char = chr(ord('a') + (ord(char_to_encrypt) - ord('a') - (n + m)) % 26)
+            
+    elif 'A' <= char_to_encrypt <= 'M':  
+            new_char = chr(ord('A') + (ord(char_to_encrypt) - ord('A') - n) % 26)
 
-        
+    elif 'N' <= char_to_encrypt <= 'Z':  
+            new_char = chr(ord('A') + (ord(char_to_encrypt) - ord('A') + (m**2)) % 26)    
+                
+    else:
+            new_char = char_to_encrypt
+
+    encrypted_text += new_char
+
+return encrypted_text
+
+def main():
+    with open("raw_text.txt", "r") as file:
+        original_text = file.read()
+
+n = int(input("enter the value for n: "))
+m = int(input("Enter the value for m: "))

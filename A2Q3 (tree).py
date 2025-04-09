2b8a7b71b1951e6fbd3facfa41b20d3bad08503a
williams-grant <grant0421@gmail.com>
2025-04-09 21:34:01 +1000

    Update A2Q3 (tree).py

--- A2Q3 (tree).py
+++ A2Q3 (tree).py
@@ -1,17 +1,23 @@
 import turtle
 
-def draw_tree(branch_length, angle_left, angle_right, reduction_factor): #Needs one more variable for number of branches remaining
-    if reduction_factor == 0: ###This needs to be testing number of branches remaining
+def draw_tree(branch_length, angle_left, angle_right, reduction_factor, recursion_depth, initial_recursion_depth): 
+    if recursion_depth == 0: 
         return
 
+    turtle.pensize(recursion_depth)
+    if recursion_depth == initial_recursion_depth:
+        turtle.pencolor("brown")
+    else:
+        turtle.pencolor("green")
+        
     turtle.forward(branch_length)
     turtle.left(angle_left)
 
-    draw_tree(branch_length * reduction_factor, angle_left, angle_right, reduction_factor) ###with additional variable for number of branches remaining, add this to call as number of branches - 1
+    draw_tree(branch_length * reduction_factor, angle_left, angle_right, reduction_factor, recursion_depth - 1, initial_recursion_depth)
 
     turtle.right(angle_left + angle_right)
 
-    draw_tree(branch_length * reduction_factor, angle_left, angle_right, reduction_factor) ###with additional variable for number of branches remaining, add this to call as number of branches - 1
+    draw_tree(branch_length * reduction_factor, angle_left, angle_right, reduction_factor, recursion_depth - 1, initial_recursion_depth)
 
     turtle.left(angle_right)
     turtle.backward(branch_length)
@@ -22,7 +28,7 @@ def main():
     angle_right = float(input("Enter right branch angle: "))
     branch_length = float(input("Enter starting branch length: "))
     reduction_factor = float(input("Enter branch length reduction factor (e.g., 0.7 for 70%): "))
-    ###Need one more input for number of branches (recursion depth)
+    initial_recursion_depth = float(input("Enter number of times to branch out: "))
 
     # Setting up turtle
     turtle.speed("fastest")
@@ -31,7 +37,7 @@ def main():
     turtle.goto(0, -200)
     turtle.down()
 
-    draw_tree(branch_length, angle_left, angle_right, reduction_factor)
+    draw_tree(branch_length, angle_left, angle_right, reduction_factor, initial_recursion_depth, initial_recursion_depth)
 
     turtle.done()
 

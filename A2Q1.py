01693e82cd09ae4a276a7b7dcfae25ea29476c67
williams-grant <grant0421@gmail.com>
2025-04-23 11:55:01 +1000

    Update A2Q1.py

--- A2Q1.py
+++ A2Q1.py
@@ -16,32 +16,31 @@ def encrypt(original_text, n, m):
         
         # if character is in range a to m, shift forward by (n*m) then return a character based on encryption logic. Mark character with '1' to indicate which rule was used
         if 'a' <= char_to_encrypt <= 'm':
-            new_char = chr(ord('a') + (ord(char_to_encrypt) - ord('a') + (n * m)) % 26)
+            encrypted_char = chr(ord('a') + (ord(char_to_encrypt) - ord('a') + (n * m)) % 26)
             key_char = '1'
 
-        #elif functions tells program to continue checking next character range when previous if statement is false
         #if character is in range n to z, shift backwards by (n + m) then return character based on encrpytion logic. Mark character with '2' to indicate logic used
         elif 'n' <= char_to_encrypt <= 'z': 
-            new_char = chr(ord('a') + (ord(char_to_encrypt) - ord('a') - (n + m)) % 26)
+            encrypted_char = chr(ord('a') + (ord(char_to_encrypt) - ord('a') - (n + m)) % 26)
             key_char = '2'
 
         #if character is in range A to M, shift backwards by ( - n) then return character based on encryption logic. Mark character with '3' to indicate logic used
         elif 'A' <= char_to_encrypt <= 'M':  
-            new_char = chr(ord('A') + (ord(char_to_encrypt) - ord('A') - n) % 26)
+            encrypted_char = chr(ord('A') + (ord(char_to_encrypt) - ord('A') - n) % 26)
             key_char = '3'
             
         #if character is in range N to Z, shift forward by (m^2) then return charter based on encryption logic. Mark character with '3' to inidcate logic used
         elif 'N' <= char_to_encrypt <= 'Z':  
-            new_char = chr(ord('A') + (ord(char_to_encrypt) - ord('A') + (m**2)) % 26) 
+            encrypted_char = chr(ord('A') + (ord(char_to_encrypt) - ord('A') + (m**2)) % 26) 
             key_char = '4'
 
         #All other charcters, remain the same and mark with '0'
         else:
-            new_char = char_to_encrypt
+            encrypted_char = char_to_encrypt
             key_char = '0'
 
         #returns transformed characters to a result string as encrypted_text and stores key codes in key_text
-        encrypted_text += new_char
+        encrypted_text += encrypted_char
         key_text += key_char
     return encrypted_text, key_text
 
@@ -58,23 +57,23 @@ def decrypt(encrypted_text, key_text, n, m):
 
         #Uses decryption logic based on key code, to reverse encryption formula. Shifting backwards by (n*m)
         if key_char == '1':
-            new_char = chr(ord('a') + (ord(char_to_decrypt) - ord('a') - (n * m)) % 26)
+            decrypted_char = chr(ord('a') + (ord(char_to_decrypt) - ord('a') - (n * m)) % 26)
             
         #if key is not 1, 'else if' statement tells program to move and decrypt based on key code. Shifting forwards by (n+m)
         elif key_char == '2': 
-            new_char = chr(ord('a') + (ord(char_to_decrypt) - ord('a') + (n + m)) % 26)
+            decrypted_char = chr(ord('a') + (ord(char_to_decrypt) - ord('a') + (n + m)) % 26)
             
         #if key code is 3, use below decyription logic to shift forward by ( +n)
         elif key_char == '3':  
-            new_char = chr(ord('A') + (ord(char_to_decrypt) - ord('A') + n) % 26)
+            decrypted_char = chr(ord('A') + (ord(char_to_decrypt) - ord('A') + n) % 26)
         #if key code is 4, use below decryption logic to shift backwards by (m^2)
         elif key_char == '4':  
-            new_char = chr(ord('A') + (ord(char_to_decrypt) - ord('A') - (m**2)) % 26)   
+            decrypted_char = chr(ord('A') + (ord(char_to_decrypt) - ord('A') - (m**2)) % 26)   
         #if key code is not 1-4 then return same character in decryption_text
         else:
-            new_char = char_to_decrypt
+            decrypted_char = char_to_decrypt
         #gives a resulting decryption string, and returns as decryption_text
-        decrypted_text += new_char 
+        decrypted_text += decrypted_char 
     return decrypted_text
 
 
